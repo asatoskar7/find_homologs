72b4d31cede702cd81ceb000a0f32c65958e1c01
asatoskar7 <anoushka.satoskar@gmail.com>
2023-09-11 11:24:40 -0400

    changed blast command

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tblastn -query $1 -subject $2 -outfmt'6 std qlen' -task blast-short | awk '$3>=30 && $4>=90' >> $3
+tblastn -query $1 -subject $2 -outfmt '6 std qlen' -task blast-short | awk '$3>=30 && $4>=90' >> $3
 wc -l $3
 
